54174d42c8ae443a21fe081068462b78890724fc
goraasep <gora.asep@gmail.com>
2024-12-16 05:49:44 +0700

    feat: finishing event organizer

--- src/app/(user_dashboard)/organizer_event/components/TicketCard/index.tsx
+++ src/app/(user_dashboard)/organizer_event/components/TicketCard/index.tsx
@@ -23,44 +23,49 @@ const TicketCard: FC<TicketCardProps> = ({
   onClose,
   onDelete,
   isReleased,
-  isClosed
-  
+  isClosed,
 }) => {
-  
-
-
   return (
     <div className="border border-platinum hover:cursor-pointer hover:bg-azureish-white rounded-lg py-2 px-5 text-center w-fit">
       <div className="font-semibold text-lg">{title}</div>
       <div>Available : {available}</div>
-      <div>Sold: {sold}</div>
+      <div>Sold : {sold}</div>
       <div>{price}</div>
       <div className="flex gap-2 mt-2 flex-wrap justify-center">
-      <button
-          className={`${isReleased ? "bg-slate-500" : "bg-true-blue"} rounded-lg py-2 px-5 text-white`}
+        <button
+          className={`${
+            isReleased ? "bg-slate-500" : "bg-true-blue"
+          } rounded-lg py-2 px-5 text-white`}
           onClick={onEdit}
           disabled={isReleased ? true : false} // Disable edit button if released
         >
           Edit
         </button>
         <button
-          className={`${isReleased ? "bg-slate-500" : "bg-true-blue"} rounded-lg py-2 px-5 text-white`}
+          className={`${
+            isReleased ? "bg-slate-500" : "bg-true-blue"
+          } rounded-lg py-2 px-5 text-white`}
           onClick={onRelease}
           disabled={isReleased ? true : false} // Disable release button if already released
         >
-          {isReleased ? "Released" : "Release"} {/* Change button text based on release status */}
+          {isReleased ? "Released" : "Release"}{" "}
+          {/* Change button text based on release status */}
         </button>
         <button
-          className={`${isClosed ? "bg-slate-500" : "bg-red-700"} rounded-lg py-2 px-5 text-white `}
+          className={`${
+            isClosed ? "bg-slate-500" : "bg-red-700"
+          } rounded-lg py-2 px-5 text-white `}
           onClick={onClose}
         >
           Close
         </button>
         <button
           // className="border border-red-500 rounded-lg py-2 px-5 text-white bg-red-700"
-          className={`${isReleased ? "bg-slate-500" : "bg-red-700"} rounded-lg py-2 px-5 text-white `}
+          className={`${
+            isReleased ? "bg-slate-500" : "bg-red-700"
+          } rounded-lg py-2 px-5 text-white `}
           onClick={onDelete}
-          disabled={isReleased ? true : false} 
+          disabled={isReleased ? true : false}
         >
           Delete
         </button>
